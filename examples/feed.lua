02c3b5951a925ba950925de094d951a5bd5fa2a3
Luna <git@l4.pm>
2024-12-28 17:03:46 -0300

    update example feed

--- examples/feed.lua
+++ examples/feed.lua
@@ -1,8 +1,23 @@
+local debug = 0
 return {
-  filter = function(post)
-    print("shit")
-    print("ASlkdjsalkdjf2")
-    print(post)
-    return false
+  filter = function(ctx)
+    local hasSubstring = string.find(string.lower(ctx.post.text), "amogus") ~= nil
+    if debug == 1 then
+      print('== POST')
+      for key, value in pairs(ctx.post) do
+          print(key, value)
+      end
+
+      print('== YOU FOLLOW')
+      for key, value in pairs(ctx.follows) do
+          print(key, value)
+      end
+
+      print('== THEY FOLLOW')
+      for key, value in pairs(ctx.followed) do
+          print(key, value)
+      end
+    end
+    return hasSubstring
   end,
 }
